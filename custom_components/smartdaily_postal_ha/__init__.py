4f570164ef71b97a2d44bc771c71e68034949ac3
andyching168 <53651198+andyching168@users.noreply.github.com>
2024-09-06 01:01:33 +0800

    修改 __init__.py以符合HA最佳實作
    詳細:
    https://developers.home-assistant.io/blog/2024/06/12/async_forward_entry_setups/

--- custom_components/smartdaily_postal_ha/__init__.py
+++ custom_components/smartdaily_postal_ha/__init__.py
@@ -1,16 +1,8 @@
-"""Example of a custom component for a package tracker."""
 
 DOMAIN = "smartdaily_postal_ha"
 
 async def async_setup_entry(hass, config_entry):
     """Set up smartdaily_postal_ha from a config entry."""
-    # Forward the setup to the sensor platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
-    )
-    # Forward the setup to the camera platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "camera")
-    )
+    # Forward the setup to the sensor and camera platforms
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor", "camera"])
     return True
-
